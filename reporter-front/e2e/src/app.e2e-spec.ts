7559cb333734f780815077c2b8f116d6ecac977d
sphall <simonhall37@hotmail.com>
2018-12-03 12:26:05 +0100

    Updated e2e test to check title of login and change baseurl

--- reporter-front/e2e/src/app.e2e-spec.ts
+++ reporter-front/e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display login message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to reporter-front!');
+    expect(page.getParagraphText()).toEqual('Login');
   });
 });
